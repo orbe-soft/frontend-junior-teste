e15e5fee2ce8f71092ba6c4f68385df3310f2c28
brunowanderson7 <bruno.lopes@ufpi.edu.br>
2024-08-19 19:24:17 -0300

    refactor: base layout

--- bike-store/src/app/layout.tsx
+++ bike-store/src/app/layout.tsx
@@ -1,26 +1,33 @@
-import type { Metadata } from 'next';
-import { Sora } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Sora } from "next/font/google";
 
-const sora = Sora({ subsets: ['latin'], weight: ['300', '400', '600', '700'] });
+import "./globals.css";
+import { Header } from "@/components/header/header";
+
+const sora = Sora({ subsets: ["latin"], weight: ["300", "400", "600", "700"] });
 
 export const metadata: Metadata = {
-  title: 'Bike Store',
+  title: "Bike Store",
   description:
-    'A Bike Store é a sua loja online especializada em bicicletas de alta qualidade, acessórios e peças. Oferecemos uma vasta seleção de modelos para todos os estilos de ciclistas, desde iniciantes até profissionais. Compre com segurança e aproveite nossas promoções exclusivas. Acelere com a Bike Store! 🚴‍♂️',
+    "A Bike Store é a sua loja online especializada em bicicletas de alta qualidade, acessórios e peças. Oferecemos uma vasta seleção de modelos para todos os estilos de ciclistas, desde iniciantes até profissionais. Compre com segurança e aproveite nossas promoções exclusivas. Acelere com a Bike Store! 🚴‍♂️",
   keywords:
-    'bike, bicicleta, loja, online, comprar, acessórios, peças, promoção, qualidade, segurança',
-  authors: [{ name: 'Bruno Wanderson', url: 'brunowand.vercel.app' }]
+    "bike, bicicleta, loja, online, comprar, acessórios, peças, promoção, qualidade, segurança",
+  authors: [{ name: "Bruno Wanderson", url: "brunowand.vercel.app" }],
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={sora.className}>{children}</body>
+      <body className={sora.className}>
+        <main className="bg-background min-h-screen w-full">
+          <Header />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
