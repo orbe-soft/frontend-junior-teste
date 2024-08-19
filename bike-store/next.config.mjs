f71c2c63d2999b3299b3340b18cf5b7437b156d6
brunowanderson7 <bruno.lopes@ufpi.edu.br>
2024-08-19 19:16:22 -0300

    refactor: add remotePatterns from images

--- bike-store/next.config.mjs
+++ bike-store/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+        port: '',
+        pathname: '/640/480/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
